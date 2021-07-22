ab5743435e2ed7282d44368a15058f583fec293e
aryanben <a.bendrey@saedubai.com>
2021-07-23 01:16:48 +0530

    added the laser traps in this project

---
+++ Assets/Scripts/LaserTrapScripts/LaserTrapScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrapScript : MonoBehaviour
+{
+    LineRenderer lr;
+    GameObject firePE;
+
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        firePE = transform.Find("FirePE").gameObject;
+        firePE.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit rHit;
+        if (Physics.Raycast(transform.position, transform.forward, out rHit))
+        {
+            if (rHit.collider)
+            {
+                lr.SetPosition(1, rHit.point);
+                if (rHit.collider.tag == "Player")
+                {
+
+
+                    firePE.SetActive(true);
+                    firePE.transform.SetPositionAndRotation(rHit.point, Quaternion.identity);
+
+
+                }
+                else if (firePE.activeSelf  )
+                {
+                    firePE.SetActive(false);
+                }
+
+            }
+
+        }
+        else
+        {
+            lr.SetPosition(1, transform.position + (transform.forward * 5000));
+            firePE.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/LaserTrapScripts/LaserTrapShooter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrapShooter : MonoBehaviour
+{
+    [SerializeField] Camera mainCam;
+    public GameObject laserTrap;
+    bool trapShot;
+    bool trapActivated;
+    GameObject tempTrap;
+    void Start()
+    {
+        trapShot = false;
+        trapActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!trapShot)
+            {
+                Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit raycastHit))
+                {
+                    tempTrap = Instantiate(laserTrap, raycastHit.point, Quaternion.identity);
+                    trapShot = true;
+                    
+                }
+            }
+            else if (trapShot)
+            {
+                Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit raycastHit))
+                {
+                    tempTrap.transform.LookAt(raycastHit.point);
+                    trapShot = false;
+
+                }
+                
+            }
+
+        }
+    }
+}
