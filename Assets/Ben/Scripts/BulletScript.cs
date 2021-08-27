dfe54b68fc1cc2a7d0fd0999ac9eed1d95603e2d
aryanben <a.bendrey@saedubai.com>
2021-08-28 01:04:02 +0530

    win lose end game script added, hp and laser ammo added, user script made into one instead of two, laser does more damage

--- Assets/Ben/Scripts/BulletScript.cs
+++ Assets/Ben/Scripts/BulletScript.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float speed;
+    public float bulletDamage;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward* speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
     private void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.transform.tag == "Player")
+        {
+            collision.gameObject.GetComponent<UserScript>().hp -= bulletDamage;
+        }
         Destroy(gameObject);
     }
 }
